5a19758054e62e44e7c9c02c292c2840658ac44d
izzycode <marcemc@yahoo.com>
2016-10-31 15:29:03 -0400

    Implement show_article feature

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -25,8 +25,8 @@ RSpec.feature "Creating Articles" do
     click_button "Create Article"
 
     expect(page).to have_content("Article was not created.")
-    expect(page).to have_content("Title can't be blank.")
-    expect(page).to have_content("Body can't be blank.")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
   end
 
 end
